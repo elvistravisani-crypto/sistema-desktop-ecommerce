2ae0f9ba7aa650844b4bdb4d0934d218cd3ee119
elvistravisani-crypto <elvistravisani@gmail.com>
2026-09-02 10:56:53 -0300

    iniciando a criação filtros

--- capivaras_hardware/classFuncionario.cs
+++ capivaras_hardware/classFuncionario.cs
@@ -89,18 +89,105 @@ namespace capivaras_hardware
 
 
         }
+        //CONSULTA DE FUNCIONÁRIO POR CARGO
+        public DataTable ConsFuncCargo(int cargo)
+        {
+            string sql = $"SELECT funcionario.codigo_funcionario 'ID', funcionario.nome 'Nome', funcionario.cpf 'CPF', funcionario.data_nascimento 'Nascimento', cargo.nome 'Cargo', funcionario.sexo 'Sexo', funcionario.cidade 'Cidade', funcionario.telefone_celular 'Celular' FROM funcionario JOIN cargo ON funcionario.codigo_cargo = cargo.codigo_cargo WHERE funcionario.status = 1 AND funcionario.codigo_cargo = {cargo} ORDER BY funcionario.nome;";
 
+            classConexao cConexao = new classConexao();
+            return cConexao.RetornaDados(sql);
+        }
 
+        //CONSULTA DE FUNCIONÁRIO POR CARGO
+        public DataTable ConsFunCpf(string cpf)
+        {
+            string sql = $"SELECT funcionario.codigo_funcionario 'ID', funcionario.nome 'Nome', funcionario.cpf 'CPF', funcionario.data_nascimento 'Nascimento', cargo.nome 'Cargo', funcionario.sexo 'Sexo', funcionario.cidade 'Cidade', funcionario.telefone_celular 'Celular' FROM funcionario JOIN cargo ON funcionario.codigo_cargo = cargo.codigo_cargo WHERE funcionario.status = 1 AND funcionario.cpf = '{cpf}' ORDER BY funcionario.nome;";
 
+            classConexao cConexao = new classConexao();
+            return cConexao.RetornaDados(sql);
+        }
+        //CONSULTA DE FUNCIONÁRIO POR  DATA DE ADMISSÃO(CADASTRO)
+        public DataTable ConsFuncDataAdmissao(DateTime datai, DateTime dataf)
+        {
+            string sql = $"SELECT funcionario.codigo_funcionario 'ID', funcionario.nome 'Nome', funcionario.cpf 'CPF', funcionario.data_nascimento 'Nascimento', cargo.nome 'Cargo', funcionario.sexo 'Sexo', funcionario.cidade 'Cidade', funcionario.telefone_celular 'Celular' FROM funcionario JOIN cargo ON funcionario.codigo_cargo = cargo.codigo_cargo WHERE funcionario.status = 1 AND CAST(funcionario.data_cadastro AS DATE) BETWEEN '{datai.ToString("yyyy-MM-dd")}' AND '{dataf.ToString("yyyy-MM-dd")}'  ORDER BY funcionario.nome;";
 
+            classConexao cConexao = new classConexao();
+            return cConexao.RetornaDados(sql);
+        }
+        //CONSULTA DE FUNCIONÁRIO POR STATUS
+        public DataTable ConsFunStatus(int status)
+        {
+            string sql = $"SELECT funcionario.codigo_funcionario 'ID', funcionario.nome 'Nome', funcionario.cpf 'CPF', funcionario.data_nascimento 'Nascimento', cargo.nome 'Cargo', funcionario.sexo 'Sexo', funcionario.cidade 'Cidade', funcionario.telefone_celular 'Celular' FROM funcionario JOIN cargo ON funcionario.codigo_cargo = cargo.codigo_cargo WHERE funcionario.status = {status} ORDER BY funcionario.nome;";
 
+            classConexao cConexao = new classConexao();
+            return cConexao.RetornaDados(sql);
+        }
+        //MÉTODO PARA BUSCAR TODOS OS DADOS DO FUNCIONÁRIO SELECIONADO PELO USUÁRIO NO FORM DE CONSULTA
+        public bool DadosFuncionario(int codigo)
+        {
+            string sql = $"SELECT * FROM funcionario WHERE codigo_funcionario = {codigo}";
 
+            classConexao classConexao = new classConexao();
 
+            //MONTAR O DATABLE QUE RECEBERÁ TODOS OS DADOS DO FUNCIONÁRIO ESCOLHIDO PELO USUÁRIO
+            DataTable dt = classConexao.RetornaDados(sql);
 
+            //SE A CONSULTA RETORNOU DADOS
+            if (dt.Rows.Count > 0)
+            {
+                //EXIBIR TODOS OS CAMOS DA TABELA FUNCIONARIO
+                codigo_funcionario = Convert.ToInt32(dt.Rows[0]["codigo_funcionario"]);
+                nome = Convert.ToString(dt.Rows[0]["nome"]);
+                nome_social = Convert.ToString(dt.Rows[0]["nome_social"]);
+                foto = Convert.ToString(dt.Rows[0]["foto"]);
+                data_nascimento = Convert.ToDateTime(dt.Rows[0]["data_nascimento"]);
+                sexo = Convert.ToString(dt.Rows[0]["sexo"]);
+                estado_civil = Convert.ToString(dt.Rows[0]["estado_civil"]);
+                cpf = Convert.ToString(dt.Rows[0]["cpf"]);
+                rg = Convert.ToString(dt.Rows[0]["rg"]);
+                salario = Convert.ToDecimal(dt.Rows[0]["salario"]);
+                endereco = Convert.ToString(dt.Rows[0]["endereco"]);
+                numero = Convert.ToInt32(dt.Rows[0]["numero"]);
+                complemento = Convert.ToString(dt.Rows[0]["complemento"]);
+                bairro = Convert.ToString(dt.Rows[0]["bairro"]);
+                cidade = Convert.ToString(dt.Rows[0]["cidade"]);
+                estado = Convert.ToString(dt.Rows[0]["estado"]);
+                cep = Convert.ToString(dt.Rows[0]["cep"]);
+                telefone_residencial = Convert.ToString(dt.Rows[0]["telefone_residencial"]);
+                telefone_celular = Convert.ToString(dt.Rows[0][" telefone_celular"]);
+                email = Convert.ToString(dt.Rows[0]["email"]);
+                usuario = Convert.ToString(dt.Rows[0]["usuario"]);
+                senha = Convert.ToString(dt.Rows[0]["senha"]);
+                tipo_acesso = Convert.ToInt32(dt.Rows[0]["tipo_acesso"]);
+                status = Convert.ToInt32(dt.Rows[0]["status"]);
+                data_cadastro = Convert.ToDateTime(dt.Rows[0]["data_cadastro"]);
+                codigo_cargo = Convert.ToInt32(dt.Rows[0]["status"]);
+                return true;
 
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //MÉTODO PARA ATUALIZAR FUNCIONÁRIO
+        public int AtualizarFuncionario()
+        {
+           string sql =  $"UPDATE funcionario SET codigo_cargo = { codigo_cargo}, nome = '{nome}', nome_social= '{nome_social}', foto = '{foto}', data_nascimento ='{data_nascimento.ToString("yyyy-MM-dd")}', sexo = '{sexo}', estado_civil = '{estado_civil}', cpf = '{cpf}', rg = '{rg}', salario = '{salario.ToString().Replace(",", ".")}', endereco = '{endereco}', numero = { numero},  complemento = '{complemento}', bairro = '{bairro}', cidade = '{cidade}', estado = '{estado}', cep = '{cep}', telefone_residencial = '{telefone_residencial}', telefone_celular = '{telefone_celular}', email = '{email}', usuario =  '{usuario}', senha = '{senha}', tipo_acesso = { tipo_acesso}, status = { status} WHERE codigo_funcionario = { codigo_funcionario };";
 
+        classConexao cConexao = new classConexao();
+        return cConexao.ExecutaQuery(sql);
 
+        }
 
+        //MÉTODO PARA EXCLUIR FUNCIONÁRIO
+
+        public int ExcluirFuncionario()
+        {
+            string sql = $"DELETE FROM funcionario WHERE codigo_funcionario = {codigo_cargo}";
+            classConexao cConexao = new classConexao();
+            return cConexao.ExecutaQuery(sql);
+        }
 
 
 
